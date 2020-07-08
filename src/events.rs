5d01a8aa7af9773aaff9f20087fdf71daf2b807c
lemunozm <lemunozm@gmail.com>
2020-07-08 02:09:29 +0200

    Timers finished correctly

--- src/events.rs
+++ src/events.rs
@@ -1,8 +1,11 @@
 use crossbeam_channel::{self, Sender, Receiver};
 
+use std::sync::{Arc, atomic::{AtomicBool, Ordering}};
 use std::time::Duration;
 use std::thread::{self, JoinHandle};
 use std::collections::{HashMap};
+
+const TIMER_SAMPLING_CHECK: u64 = 50; //ms
 
 pub enum Event<Message, Signal, Endpoint>
 {
@@ -12,12 +15,14 @@ pub enum Event<Message, Signal, Endpoint>
     Signal(Signal),
 }
 
-pub struct EventQueue<E> {
+pub struct EventQueue<E>
+where E: Send + 'static {
     receiver: Receiver<E>,
     event_sender: EventSender<E>,
 }
 
-impl<E: Send + 'static> EventQueue<E> {
+impl<E> EventQueue<E>
+where E: Send + 'static {
     pub fn new() -> EventQueue<E>
     {
         let (sender, receiver) = crossbeam_channel::unbounded();
@@ -41,9 +46,11 @@ impl<E: Send + 'static> EventQueue<E> {
 }
 
 
-pub struct EventSender<E> {
+pub struct EventSender<E>
+where E: Send + 'static {
     sender: Sender<E>,
     timer_registry: HashMap<usize, JoinHandle<()>>,
+    timers_running: Arc<AtomicBool>,
     last_timer_id: usize,
 }
 
@@ -53,6 +60,7 @@ where E: Send + 'static {
         EventSender {
             sender,
             timer_registry: HashMap::new(),
+            timers_running: Arc::new(AtomicBool::new(true)),
             last_timer_id: 0,
         }
     }
@@ -64,32 +72,45 @@ where E: Send + 'static {
     pub fn send_with_timer(&mut self, event: E, duration: Duration) {
         let sender = self.sender.clone();
         let timer_id = self.last_timer_id;
+        let running = self.timers_running.clone();
+        let mut time_acc = Duration::from_secs(0);
+        let duration_step = Duration::from_millis(TIMER_SAMPLING_CHECK);
         let timer_handle = thread::spawn(move || {
-            thread::sleep(duration);
+            while time_acc < duration {
+                thread::sleep(duration_step);
+                time_acc += duration_step;
+                if !running.load(Ordering::Relaxed) {
+                    return;
+                }
+            }
             sender.send(event).unwrap();
         });
         self.timer_registry.insert(timer_id, timer_handle);
         self.last_timer_id += 1;
     }
 
-    pub fn stop_timers(&self) {
-        todo!();
+    pub fn stop_timers(&mut self) {
+        self.timers_running.store(false, Ordering::Relaxed);
     }
 }
 
-impl<E> Drop for EventSender<E> {
+impl<E> Drop for EventSender<E>
+where E: Send + 'static {
     fn drop(&mut self) {
+        self.stop_timers();
         for (_, timer) in self.timer_registry.drain() {
             timer.join().unwrap();
         }
     }
 }
 
-impl<E> Clone for EventSender<E> {
+impl<E> Clone for EventSender<E>
+where E: Send + 'static {
     fn clone(&self) -> Self {
         Self {
             sender: self.sender.clone(),
             timer_registry: HashMap::new(),
+            timers_running: Arc::new(AtomicBool::new(true)),
             last_timer_id: 0,
         }
     }

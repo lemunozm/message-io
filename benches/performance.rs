fa13f7f25da59a1a27c7f88404311a785853e99c
lemunozm <lemunozm@gmail.com>
2020-11-30 01:12:41 +0100

    Fixed std-ref

--- benches/performance.rs
+++ benches/performance.rs
@@ -41,47 +41,22 @@ enum Transport {
     Udp,
 }
 
-/// Measure the cost of sending a message using std::net::TcpStream
-fn send_message_std_tcp<M>(c: &mut Criterion, message: M)
-where M: Serialize + for<'b> Deserialize<'b> + Send + Copy + 'static {
-    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
-    let addr = listener.local_addr().unwrap();
-
-    // Created only for not saturate the stream
-    std::thread::spawn(move || {
-        let mut receiver_stream = listener.incoming().next().unwrap().unwrap();
-        loop {
-            if let Ok(0) = receiver_stream.read(&mut [0; BIG_SIZE]) {
-                break
-            }
-        }
-    });
-
-    let stream = TcpStream::connect(addr).unwrap();
-    let mut buffer = Vec::with_capacity(std::mem::size_of::<M>());
-
-    let msg = format!("Sending {} bytes by Tcp (std)", std::mem::size_of::<M>());
-    c.bench_function(&msg, |b| {
-        b.iter(|| {
-            buffer.clear();
-            bincode::serialize_into(&mut buffer, &message).unwrap();
-            (&stream).write(&buffer).unwrap();
-        });
-    });
-}
+//======================================================================
+//                     std benches (used as reference)
+//======================================================================
 
 /// Measure the cost of sending a message using std::net::UdpSocket
-fn send_message_std_udp<M>(c: &mut Criterion, message: M)
+fn std_send_udp<M>(c: &mut Criterion, message: M)
 where M: Serialize + for<'b> Deserialize<'b> + Send + Copy + 'static {
-    let receiver = UdpSocket::bind("127.0.0.1:0").unwrap();
-    let addr = receiver.local_addr().unwrap();
-
-    let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
-    socket.connect(addr).unwrap();
-    let mut buffer = Vec::with_capacity(std::mem::size_of::<M>());
-
-    let msg = format!("Sending {} bytes by Udp (std)", std::mem::size_of::<M>());
+    let msg = format!("std-ref: Sending {} bytes by Udp", std::mem::size_of::<M>());
     c.bench_function(&msg, |b| {
+        let receiver = UdpSocket::bind("127.0.0.1:0").unwrap();
+        let addr = receiver.local_addr().unwrap();
+
+        let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+        socket.connect(addr).unwrap();
+        let mut buffer = Vec::with_capacity(std::mem::size_of::<M>());
+
         b.iter(|| {
             buffer.clear();
             bincode::serialize_into(&mut buffer, &message).unwrap();
@@ -90,66 +65,48 @@ where M: Serialize + for<'b> Deserialize<'b> + Send + Copy + 'static {
     });
 }
 
-/// Measure the cost of sending a message by message-io
-fn send_message<M>(c: &mut Criterion, message: M, transport: Transport)
+/// Measure the cost of sending a message using std::net::TcpStream
+fn std_send_tcp<M>(c: &mut Criterion, message: M)
 where M: Serialize + for<'b> Deserialize<'b> + Send + Copy + 'static {
-    // We need the internal network thread running while sending messages
-    let mut recv_network = NetworkManager::new(|_: NetEvent<M>| ());
-
-    let receiver_addr = match transport {
-        Transport::Tcp => recv_network.listen_tcp("127.0.0.1:0").unwrap().1,
-        Transport::Udp => recv_network.listen_udp("127.0.0.1:0").unwrap().1,
-    };
-
-    let mut send_network = NetworkManager::new(|_: NetEvent<M>| ());
-
-    let receiver = match transport {
-        Transport::Tcp => send_network.connect_tcp(receiver_addr).unwrap(),
-        Transport::Udp => send_network.connect_udp(receiver_addr).unwrap(),
-    };
-
-    let msg = format!("Sending {} bytes by {:?}", std::mem::size_of::<M>(), transport);
+    let msg = format!("std-ref: Sending {} bytes by Tcp", std::mem::size_of::<M>());
     c.bench_function(&msg, |b| {
-        b.iter(|| {
-            // The following process encodes, serializes and sends:
-            send_network.send(receiver, message).unwrap();
+        let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+        let addr = listener.local_addr().unwrap();
+
+        // Created only for not saturate the stream
+        let receiver_handle = std::thread::spawn(move || {
+            let mut receiver_stream = listener.incoming().next().unwrap().unwrap();
+            loop {
+                if let Ok(0) = receiver_stream.read(&mut [0; BIG_SIZE]) {
+                    break
+                }
+            }
         });
-    });
-}
 
-/// Measure the cost of sending a message by message-io
-/// while at same time the network is receiving messages.
-fn send_while_recv_message<M>(c: &mut Criterion, message: M, transport: Transport)
-where M: Serialize + for<'b> Deserialize<'b> + Send + Copy + 'static {
-    // The sender will send to a listener in the same network.
-    // This emulates the process of sending messages while
-    // other instace are sending also to the first one.
-    let mut network = NetworkManager::new(|_: NetEvent<M>| ());
+        let stream = TcpStream::connect(addr).unwrap();
+        let mut buffer = Vec::with_capacity(std::mem::size_of::<M>());
 
-    let receiver_addr = match transport {
-        Transport::Tcp => network.listen_tcp("127.0.0.1:0").unwrap().1,
-        Transport::Udp => network.listen_udp("127.0.0.1:0").unwrap().1,
-    };
+        {
+            let stream = stream;
+            b.iter(|| {
+                buffer.clear();
+                bincode::serialize_into(&mut buffer, &message).unwrap();
+                (&stream).write(&buffer).unwrap();
+            });
+            //stream is destroyed and closed here
+        }
 
-    let receiver = match transport {
-        Transport::Tcp => network.connect_tcp(receiver_addr).unwrap(),
-        Transport::Udp => network.connect_udp(receiver_addr).unwrap(),
-    };
-
-    let msg = format!("Sending {} bytes by {:?} (while recv)", std::mem::size_of::<M>(), transport);
-
-    c.bench_function(&msg, |b| {
-        b.iter(|| {
-            // The following process encodes, serializes and sends:
-            network.send(receiver, message).unwrap();
-        });
+        receiver_handle.join().unwrap();
     });
 }
 
 /// Measure the cost of sending messages and processing it using std::net::TcpStream
-fn send_recv_tcp_std_one_direction<M>(c: &mut Criterion, message: M)
+fn std_send_recv_tcp_one_direction<M>(c: &mut Criterion, message: M)
 where M: Serialize + for<'b> Deserialize<'b> + Send + Copy + 'static {
-    let msg = format!("Sending and receiving {} bytes by Tcp (std)", std::mem::size_of::<M>());
+    let msg = format!(
+        "std-ref: Sending and receiving one direction. {} bytes by Tcp",
+        std::mem::size_of::<M>()
+    );
     c.bench_function(&msg, |b| {
         b.iter_custom(|iters| {
             let listener = TcpListener::bind("127.0.0.1:0").unwrap();
@@ -184,10 +141,70 @@ where M: Serialize + for<'b> Deserialize<'b> + Send + Copy + 'static {
     });
 }
 
+//======================================================================
+//                         message-io benches
+//======================================================================
+
+/// Measure the cost of sending a message by message-io
+fn send_message<M>(c: &mut Criterion, message: M, transport: Transport)
+where M: Serialize + for<'b> Deserialize<'b> + Send + Copy + 'static {
+    let msg = format!("Sending {} bytes by {:?}", std::mem::size_of::<M>(), transport);
+    c.bench_function(&msg, |b| {
+        // We need the internal network thread running while sending messages
+        let mut recv_network = NetworkManager::new(|_: NetEvent<M>| ());
+
+        let receiver_addr = match transport {
+            Transport::Tcp => recv_network.listen_tcp("127.0.0.1:0").unwrap().1,
+            Transport::Udp => recv_network.listen_udp("127.0.0.1:0").unwrap().1,
+        };
+
+        let mut send_network = NetworkManager::new(|_: NetEvent<M>| ());
+
+        let receiver = match transport {
+            Transport::Tcp => send_network.connect_tcp(receiver_addr).unwrap(),
+            Transport::Udp => send_network.connect_udp(receiver_addr).unwrap(),
+        };
+
+        b.iter(|| {
+            // The following process encodes, serializes and sends:
+            send_network.send(receiver, message).unwrap();
+        });
+    });
+}
+
+/// Measure the cost of sending a message by message-io
+/// while at same time the network is receiving messages.
+fn send_while_recv_message<M>(c: &mut Criterion, message: M, transport: Transport)
+where M: Serialize + for<'b> Deserialize<'b> + Send + Copy + 'static {
+    let msg = format!("Sending {} bytes by {:?} (while recv)", std::mem::size_of::<M>(), transport);
+    c.bench_function(&msg, |b| {
+        // The sender will send to a listener in the same network.
+        // This emulates the process of sending messages while
+        // other instace are sending also to the first one.
+        let mut network = NetworkManager::new(|_: NetEvent<M>| ());
+
+        let receiver_addr = match transport {
+            Transport::Tcp => network.listen_tcp("127.0.0.1:0").unwrap().1,
+            Transport::Udp => network.listen_udp("127.0.0.1:0").unwrap().1,
+        };
+
+        let receiver = match transport {
+            Transport::Tcp => network.connect_tcp(receiver_addr).unwrap(),
+            Transport::Udp => network.connect_udp(receiver_addr).unwrap(),
+        };
+
+        b.iter(|| {
+            // The following process encodes, serializes and sends:
+            network.send(receiver, message).unwrap();
+        });
+    });
+}
+
 /// Measure the cost of sending messages and processing it by message-io
 fn send_recv_tcp_one_direction<M>(c: &mut Criterion, message: M)
 where M: Serialize + for<'b> Deserialize<'b> + Send + Copy + 'static {
-    let msg = format!("Sending and receiving {} bytes by Tcp", std::mem::size_of::<M>());
+    let msg =
+        format!("Sending and receiving one direction {} bytes by Tcp", std::mem::size_of::<M>());
     c.bench_function(&msg, |b| {
         b.iter_custom(|iters| {
             let mut recv_event_queue = EventQueue::<NetEvent<M>>::new();
@@ -212,7 +229,7 @@ where M: Serialize + for<'b> Deserialize<'b> + Send + Copy + 'static {
                     if let NetEvent::Message(..) = recv_event_queue.receive() {
                         received += 1;
                         if received == iters {
-                            break;
+                            break
                         }
                     }
                 }
@@ -228,27 +245,27 @@ where M: Serialize + for<'b> Deserialize<'b> + Send + Copy + 'static {
     });
 }
 
-fn send_recv_tcp_size_std(c: &mut Criterion) {
-    send_recv_tcp_std_one_direction(c, SMALL_MESSAGE);
-    send_recv_tcp_std_one_direction(c, MEDIUM_MESSAGE);
+fn std_send_recv_tcp_size(c: &mut Criterion) {
+    std_send_recv_tcp_one_direction(c, SMALL_MESSAGE);
+    std_send_recv_tcp_one_direction(c, MEDIUM_MESSAGE);
 }
 
 fn send_recv_tcp_size(c: &mut Criterion) {
     send_recv_tcp_one_direction(c, SMALL_MESSAGE);
     send_recv_tcp_one_direction(c, MEDIUM_MESSAGE);
-    // Generates stack overflow. The sender is faster than receiver.
+    // Generates stack overflow. The sender is faster than receiver, and the EventQueue get filled.
     //send_recv_tcp_one_direction(c, BIG_MESSAGE);
 }
 
-fn send_message_std_size_transport(c: &mut Criterion) {
-    send_message_std_tcp(c, SMALL_MESSAGE);
-    send_message_std_tcp(c, MEDIUM_MESSAGE);
-    send_message_std_tcp(c, BIG_MESSAGE);
-    send_message_std_udp(c, SMALL_MESSAGE);
-    send_message_std_udp(c, MEDIUM_MESSAGE);
+fn std_send_size_transport(c: &mut Criterion) {
+    std_send_tcp(c, SMALL_MESSAGE);
+    std_send_tcp(c, MEDIUM_MESSAGE);
+    std_send_tcp(c, BIG_MESSAGE);
+    std_send_udp(c, SMALL_MESSAGE);
+    std_send_udp(c, MEDIUM_MESSAGE);
 }
 
-fn send_message_size_transport(c: &mut Criterion) {
+fn send_size_transport(c: &mut Criterion) {
     send_message(c, SMALL_MESSAGE, Transport::Tcp);
     send_message(c, MEDIUM_MESSAGE, Transport::Tcp);
     send_message(c, BIG_MESSAGE, Transport::Tcp);
@@ -256,7 +273,7 @@ fn send_message_size_transport(c: &mut Criterion) {
     send_message(c, MEDIUM_MESSAGE, Transport::Udp);
 }
 
-fn send_message_while_recv_size_transport(c: &mut Criterion) {
+fn send_while_recv_size_transport(c: &mut Criterion) {
     send_while_recv_message(c, SMALL_MESSAGE, Transport::Tcp);
     send_while_recv_message(c, MEDIUM_MESSAGE, Transport::Tcp);
     send_while_recv_message(c, BIG_MESSAGE, Transport::Tcp);
@@ -266,11 +283,11 @@ fn send_message_while_recv_size_transport(c: &mut Criterion) {
 
 criterion_group!(
     benches,
-    send_recv_tcp_size_std,
+    std_send_size_transport,
+    std_send_recv_tcp_size,
     send_recv_tcp_size,
-    send_message_std_size_transport,
-    send_message_size_transport,
-    send_message_while_recv_size_transport
+    send_size_transport,
+    send_while_recv_size_transport
 );
 
 criterion_main!(benches);

f1b8fd24c7e884f3e31d8855e7cc59fe9d774294
lemunozm <lemunozm@gmail.com>
2021-02-04 19:52:46 +0100

    Added logger to test and check the timeouts

--- tests/basic_functionality.rs
+++ tests/basic_functionality.rs
@@ -4,21 +4,30 @@ use message_io::{MAX_UDP_LEN};
 
 use std::net::{TcpStream, Shutdown};
 use std::time::{Duration};
+use std::sync::{Once};
+use simple_logger::{SimpleLogger};
 
 const SMALL_MESSAGE: &'static str = "Small message";
+const TIMEOUT: u64 = 5; //seconds
+const LOCAL_ADDR: &'static str = "127.0.0.1:0";
+
+// Used to init only one time the log;
+static INIT: Once = Once::new();
 
 #[test]
 fn simple_connection_data_disconnection_by_tcp() {
+    INIT.call_once(|| SimpleLogger::new().init().unwrap());
+
     let mut event_queue = EventQueue::<NetEvent<String>>::new();
     let sender = event_queue.sender().clone();
     let mut network = Network::new(move |net_event| sender.send(net_event));
 
-    let (_, server_addr) = network.listen(Transport::Tcp, "127.0.0.1:0").unwrap();
+    let (_, server_addr) = network.listen(Transport::Tcp, LOCAL_ADDR).unwrap();
 
     let server_handle = std::thread::spawn(move || {
         let mut client_endpoint = None;
         loop {
-            match event_queue.receive() {
+            match event_queue.receive_timeout(Duration::from_secs(TIMEOUT)).unwrap() {
                 NetEvent::Message(endpoint, message) => {
                     assert_eq!(*client_endpoint.as_ref().unwrap(), endpoint);
                     assert_eq!(message, SMALL_MESSAGE);
@@ -46,7 +55,7 @@ fn simple_connection_data_disconnection_by_tcp() {
         let server_endpoint = network.connect(Transport::Tcp, server_addr).unwrap();
         network.send(server_endpoint, SMALL_MESSAGE.to_string());
         loop {
-            match event_queue.receive() {
+            match event_queue.receive_timeout(Duration::from_secs(TIMEOUT)).unwrap() {
                 NetEvent::Message(endpoint, message) => {
                     assert_eq!(server_endpoint, endpoint);
                     assert_eq!(message, SMALL_MESSAGE);
@@ -68,17 +77,19 @@ fn simple_connection_data_disconnection_by_tcp() {
 
 #[test]
 fn simple_data_by_udp() {
+    INIT.call_once(|| SimpleLogger::new().init().unwrap());
+
     let mut event_queue = EventQueue::<NetEvent<String>>::new();
     let sender = event_queue.sender().clone();
     let mut network = Network::new(move |net_event| sender.send(net_event));
 
     let (upd_listen_resource_id, server_addr) =
-        network.listen(Transport::Udp, "127.0.0.1:0").unwrap();
+        network.listen(Transport::Udp, LOCAL_ADDR).unwrap();
 
     let server_handle = std::thread::spawn(move || {
         loop {
-            match event_queue.receive_timeout(Duration::from_secs(1)) {
-                Some(NetEvent::Message(endpoint, message)) => {
+            match event_queue.receive_timeout(Duration::from_secs(TIMEOUT)).unwrap() {
+                NetEvent::Message(endpoint, message) => {
                     assert_eq!(upd_listen_resource_id, endpoint.resource_id());
                     assert_eq!(message, SMALL_MESSAGE);
                     network.send(endpoint, message);
@@ -97,8 +108,8 @@ fn simple_data_by_udp() {
         let server_endpoint = network.connect(Transport::Udp, server_addr).unwrap();
         network.send(server_endpoint, SMALL_MESSAGE.to_string());
         loop {
-            match event_queue.receive_timeout(Duration::from_secs(1)) {
-                Some(NetEvent::Message(endpoint, message)) => {
+            match event_queue.receive_timeout(Duration::from_secs(TIMEOUT)).unwrap() {
+                NetEvent::Message(endpoint, message) => {
                     assert_eq!(server_endpoint, endpoint);
                     assert_eq!(message, SMALL_MESSAGE);
                     break //Exit from thread
@@ -114,19 +125,21 @@ fn simple_data_by_udp() {
 
 #[test]
 fn long_tcp_message() {
+    INIT.call_once(|| SimpleLogger::new().init().unwrap());
+
     let mut event_queue = EventQueue::<NetEvent<Vec<u8>>>::new();
     let sender = event_queue.sender().clone();
     let mut network = Network::new(move |net_event| sender.send(net_event));
 
     const MESSAGE_SIZE: usize = 1_000_000; // Arround 1MB
     const VALUE: u8 = 0xAA;
-    let (_, receiver_addr) = network.listen(Transport::Tcp, "127.0.0.1:0").unwrap();
+    let (_, receiver_addr) = network.listen(Transport::Tcp, LOCAL_ADDR).unwrap();
 
     let receiver_handle = std::thread::spawn(move || {
         // Pass the network to the thread. The network should be destroyed before event queue.
         let _ = network;
         loop {
-            match event_queue.receive() {
+            match event_queue.receive_timeout(Duration::from_secs(TIMEOUT)).unwrap() {
                 NetEvent::Message(_, message) => {
                     assert_eq!(message.len(), MESSAGE_SIZE);
                     assert!(message.iter().all(|&byte| byte == VALUE));
@@ -152,19 +165,21 @@ fn long_tcp_message() {
 
 #[test]
 fn max_udp_size_message() {
+    INIT.call_once(|| SimpleLogger::new().init().unwrap());
+
     let mut event_queue = EventQueue::<NetEvent<Vec<u8>>>::new();
     let sender = event_queue.sender().clone();
     let mut network = Network::new(move |net_event| sender.send(net_event));
 
     const MESSAGE_SIZE: usize = MAX_UDP_LEN - 8; // Vec<u8> header + encoding header
     const VALUE: u8 = 0xFF;
-    let (_, receiver_addr) = network.listen(Transport::Udp, "127.0.0.1:0").unwrap();
+    let (_, receiver_addr) = network.listen(Transport::Udp, LOCAL_ADDR).unwrap();
 
     let receiver_handle = std::thread::spawn(move || {
         // Pass the network to the thread. The network should be destroyed before event queue.
         let _ = network;
         loop {
-            match event_queue.receive() {
+            match event_queue.receive_timeout(Duration::from_secs(TIMEOUT)).unwrap() {
                 NetEvent::Message(_, message) => {
                     assert_eq!(message.len(), MESSAGE_SIZE);
                     assert!(message.iter().all(|&byte| byte == VALUE));
@@ -190,18 +205,20 @@ fn max_udp_size_message() {
 
 #[test]
 fn disconnection() {
+    INIT.call_once(|| SimpleLogger::new().init().unwrap());
+
     let mut event_queue = EventQueue::<NetEvent<Vec<u8>>>::new();
     let sender = event_queue.sender().clone();
     let mut network = Network::new(move |net_event| sender.send(net_event));
 
-    let (_, receiver_addr) = network.listen(Transport::Tcp, "127.0.0.1:0").unwrap();
+    let (_, receiver_addr) = network.listen(Transport::Tcp, LOCAL_ADDR).unwrap();
 
     let receiver_handle = std::thread::spawn(move || {
         // Pass the network to the thread. The network should be destroyed before event queue.
         let _ = network;
         let mut connected = false;
         loop {
-            match event_queue.receive() {
+            match event_queue.receive_timeout(Duration::from_secs(TIMEOUT)).unwrap() {
                 NetEvent::Message(..) => unreachable!(),
                 NetEvent::AddedEndpoint(_) => connected = true,
                 NetEvent::RemovedEndpoint(_) => {

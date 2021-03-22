98fa8d7062411fb8ba257bc72830dafbee31c8c9
lemunozm <lemunozm@gmail.com>
2021-03-22 23:38:31 +0100

    Fixed an issue where the register was not removed

--- src/driver.rs
+++ src/driver.rs
@@ -6,10 +6,7 @@ use crate::remote_addr::{RemoteAddr};
 use crate::adapter::{Adapter, Remote, Local, SendStatus, AcceptedType, ReadStatus};
 
 use std::net::{SocketAddr};
-use std::sync::{
-    Arc,
-    atomic::{AtomicBool, Ordering},
-};
+use std::sync::{Arc};
 use std::io::{self};
 
 /// Struct used to identify and event that an adapter has been produced.
@@ -42,8 +39,6 @@ pub trait EventProcessor: Send + Sync {
 pub struct Driver<R: Remote, L: Local> {
     remote_registry: Arc<ResourceRegistry<R>>,
     local_registry: Arc<ResourceRegistry<L>>,
-    remote_removal_indicator: Arc<AtomicBool>,
-    local_removal_indicator: Arc<AtomicBool>,
 }
 
 impl<R: Remote, L: Local> Driver<R, L> {
@@ -52,23 +47,12 @@ impl<R: Remote, L: Local> Driver<R, L> {
         adapter_id: u8,
         poll: &mut Poll,
     ) -> Driver<R, L> {
-        let remote_removal_indicator = Arc::new(AtomicBool::new(false));
-        let local_removal_indicator = Arc::new(AtomicBool::new(false));
-
         let remote_poll_registry = poll.create_registry(adapter_id, ResourceType::Remote);
         let local_poll_registry = poll.create_registry(adapter_id, ResourceType::Local);
 
         Driver {
-            remote_registry: Arc::new(ResourceRegistry::<R>::new(
-                remote_poll_registry,
-                remote_removal_indicator.clone(),
-            )),
-            local_registry: Arc::new(ResourceRegistry::<L>::new(
-                local_poll_registry,
-                local_removal_indicator.clone(),
-            )),
-            remote_removal_indicator,
-            local_removal_indicator,
+            remote_registry: Arc::new(ResourceRegistry::<R>::new(remote_poll_registry)),
+            local_registry: Arc::new(ResourceRegistry::<L>::new(local_poll_registry)),
         }
     }
 }
@@ -78,8 +62,6 @@ impl<R: Remote, L: Local> Clone for Driver<R, L> {
         Driver {
             remote_registry: self.remote_registry.clone(),
             local_registry: self.local_registry.clone(),
-            remote_removal_indicator: self.remote_removal_indicator.clone(),
-            local_removal_indicator: self.local_removal_indicator.clone(),
         }
     }
 }
@@ -106,7 +88,7 @@ impl<R: Remote, L: Local> ActionController for Driver<R, L> {
         match endpoint.resource_id().resource_type() {
             ResourceType::Remote => {
                 match self.remote_registry.get(endpoint.resource_id()) {
-                    Some(resource) => resource.0.send(data),
+                    Some(remote) => remote.resource.send(data),
 
                     // TODO: currently there is not a safe way to know if this is
                     // reached because of a user API error (send over already resource removed)
@@ -117,7 +99,7 @@ impl<R: Remote, L: Local> ActionController for Driver<R, L> {
                 }
             }
             ResourceType::Local => match self.local_registry.get(endpoint.resource_id()) {
-                Some(resource) => resource.0.send_to(endpoint.addr(), data),
+                Some(remote) => remote.resource.send_to(endpoint.addr(), data),
                 None => {
                     panic!(
                         "Error: You are trying to send by a local resource \
@@ -147,23 +129,17 @@ impl<R: Remote, L: Local<Remote = R>> EventProcessor for Driver<R, L> {
 
 impl<R: Remote, L: Local<Remote = R>> Driver<R, L> {
     fn process_remote(&self, id: ResourceId, event_callback: &dyn Fn(AdapterEvent<'_>)) {
-        if let Some(remote) = self.remote_registry.get(id) {
-            let guard = RemovalGuard::from(&*self.remote_removal_indicator);
-            let endpoint = Endpoint::new(id, remote.1);
+        if let Some(remote) = self.remote_registry.get_protected(id) {
+            let endpoint = Endpoint::new(id, remote.addr);
             log::trace!("Processed remote for {}", endpoint);
-            let status = remote.0.receive(&|data| {
+            let status = remote.resource.receive(&|data| {
                 event_callback(AdapterEvent::Data(endpoint, data));
             });
             log::trace!("Processed remote receive status {}", status);
 
-            // We do not want the remote access living to perform the removing.
-            drop(remote);
-            drop(guard);
-
             // The user in the callback could have removed the same resource.
-            if !self.remote_registry.remove_pending_resource() {
-                if let ReadStatus::Disconnected = status {
-                    self.remote_registry.remove(id);
+            if let ReadStatus::Disconnected = status {
+                if self.remote_registry.remove(id) {
                     event_callback(AdapterEvent::Removed(endpoint));
                 }
             }
@@ -171,10 +147,9 @@ impl<R: Remote, L: Local<Remote = R>> Driver<R, L> {
     }
 
     fn process_local(&self, id: ResourceId, event_callback: &dyn Fn(AdapterEvent<'_>)) {
-        if let Some(local) = self.local_registry.get(id) {
-            let guard = RemovalGuard::from(&*self.local_removal_indicator);
+        if let Some(local) = self.local_registry.get_protected(id) {
             log::trace!("Processed local for {}", id);
-            local.0.accept(&|accepted| {
+            local.resource.accept(&|accepted| {
                 log::trace!("Processed local accepted type {}", accepted);
                 match accepted {
                     AcceptedType::Remote(addr, remote) => {
@@ -188,30 +163,7 @@ impl<R: Remote, L: Local<Remote = R>> Driver<R, L> {
                     }
                 }
             });
-
-            drop(local);
-            drop(guard);
-
-            self.remote_registry.remove_pending_resource();
         }
-    }
-}
-
-/// Guard using for ensure that no removings are performed during the lifetime of the guard.
-struct RemovalGuard<'a> {
-    indicator: &'a AtomicBool,
-}
-
-impl<'a> From<&'a AtomicBool> for RemovalGuard<'a> {
-    fn from(indicator: &'a AtomicBool) -> Self {
-        indicator.store(true, Ordering::Relaxed);
-        Self {indicator}
-    }
-}
-
-impl<'a> Drop for RemovalGuard<'a> {
-    fn drop(&mut self) {
-        self.indicator.store(false, Ordering::Relaxed);
     }
 }
 
--- src/registry.rs
+++ src/registry.rs
@@ -3,8 +3,6 @@ use crate::poll::{PollRegistry};
 use crate::adapter::{Resource};
 use crate::util::{OTHER_THREAD_ERR};
 
-use crossbeam::channel::{self, Sender, Receiver};
-
 use std::collections::{HashMap};
 use std::net::{SocketAddr};
 use std::sync::{
@@ -12,77 +10,126 @@ use std::sync::{
     atomic::{AtomicBool, Ordering},
 };
 
+pub struct Register<S> {
+    pub resource: S,
+    pub addr: SocketAddr,
+    delayed_removal: Arc<AtomicBool>,
+    must_remove: Arc<AtomicBool>,
+}
+
+impl<S> Register<S> {
+    fn new(resource: S, addr: SocketAddr) -> Self {
+        Self {
+            resource,
+            addr,
+            delayed_removal: Arc::new(AtomicBool::new(false)),
+            must_remove: Arc::new(AtomicBool::new(false)),
+        }
+    }
+}
+
 pub struct ResourceRegistry<S> {
     // We store the most significant addr of the resource because if the resource disconnects,
     // it can not be retrieved.
     // If the resource is a remote resource, the addr will be the peer addr.
     // If the resource is a local resource, the addr will be the local addr.
-    resources: RwLock<HashMap<ResourceId, Arc<(S, SocketAddr)>>>,
-    delayed_removal_sender: Sender<Arc<(S, SocketAddr)>>,
-    delayed_removal_receiver: Receiver<Arc<(S, SocketAddr)>>,
-    delayed_removal_indicator: Arc<AtomicBool>,
+    resources: RwLock<HashMap<ResourceId, Arc<Register<S>>>>,
     poll_registry: PollRegistry,
 }
 
 impl<S: Resource> ResourceRegistry<S> {
-    pub fn new(
-        poll_registry: PollRegistry,
-        delayed_removal_indicator: Arc<AtomicBool>,
-    ) -> ResourceRegistry<S> {
-        let (delayed_removal_sender, delayed_removal_receiver) = channel::unbounded();
-        ResourceRegistry {
-            resources: RwLock::new(HashMap::new()),
-            delayed_removal_sender,
-            delayed_removal_receiver,
-            delayed_removal_indicator,
-            poll_registry,
-        }
+    pub fn new(poll_registry: PollRegistry) -> ResourceRegistry<S> {
+        ResourceRegistry { resources: RwLock::new(HashMap::new()), poll_registry }
     }
 
+    /// Add a resource into the registry.
     pub fn add(&self, mut resource: S, addr: SocketAddr) -> ResourceId {
         let id = self.poll_registry.add(resource.source());
-        self.resources.write().expect(OTHER_THREAD_ERR).insert(id, Arc::new((resource, addr)));
+        let register = Register::new(resource, addr);
+        self.resources.write().expect(OTHER_THREAD_ERR).insert(id, Arc::new(register));
         id
     }
 
+    /// Remove a register from the registry.
+    /// This function ensure that the register is removed from the registry,
+    /// but not the destruction of the resource itself.
+    /// Because the resource is shared the removing will be delayed until the last reference.
     pub fn remove(&self, id: ResourceId) -> bool {
         self.resources
             .write()
             .expect(OTHER_THREAD_ERR)
             .remove(&id)
-            .map(|resource| {
-                if self.delayed_removal_indicator.load(Ordering::Relaxed) {
-                    // We are potential removing into a nested call of the same resource,
-                    // so we can not perform 'Arc::get_mut()i the resource because it is
+            .map(|mut register| {
+                if register.delayed_removal.load(Ordering::Relaxed) {
+                    log::trace!("Remove mode: delayed");
+                    // We are potential removing into a nested call of the same registry,
+                    // so we can not perform 'Arc::get_mut()' over the registry because it is
                     // already using. We can not simply wait because is the same thread which are
                     // using the resource. We are forced to delay the removal.
-                    self.delayed_removal_sender.send(resource).unwrap();
+                    register.must_remove.store(false, Ordering::Relaxed);
                 }
                 else {
+                    log::trace!("Remove mode: blocked");
                     // We have not into a nested call using the same resource,
                     // so we need to wait to finish other thread using this resource and remove.
-                    Self::wait_to_remove(&self.poll_registry, resource);
+                    Self::wait_to_remove(&self.poll_registry, &mut register);
                 }
             })
             .is_some()
     }
 
-    pub fn get(&self, id: ResourceId) -> Option<Arc<(S, SocketAddr)>> {
+    /// Returned a shared reference of the register.
+    pub fn get(&self, id: ResourceId) -> Option<Arc<Register<S>>> {
         self.resources.read().expect(OTHER_THREAD_ERR).get(&id).cloned()
     }
 
-    pub fn remove_pending_resource(&self) -> bool {
-        self.delayed_removal_receiver.try_recv().map(|resource| {
-            Self::wait_to_remove(&self.poll_registry, resource);
-        }).ok().is_some()
+    /// As get, but the register is returned protected against removings.
+    /// If a removed is performed during the lifetime of `ProtectedResource`,
+    /// the removing is delayed until its drop.
+    pub fn get_protected(&self, id: ResourceId) -> Option<ProtectedResource<S>> {
+        self.resources
+            .read()
+            .expect(OTHER_THREAD_ERR)
+            .get(&id)
+            .map(|register| ProtectedResource::new(register.clone(), &self.poll_registry))
     }
 
-    fn wait_to_remove(poll_registry: &PollRegistry, mut resource: Arc<(S, SocketAddr)>) {
+    fn wait_to_remove(poll_registry: &PollRegistry, register: &mut Arc<Register<S>>) {
         loop {
-            match Arc::get_mut(&mut resource) {
-                Some(resource) => break poll_registry.remove(resource.0.source()),
+            match Arc::get_mut(register) {
+                Some(register) => break poll_registry.remove(register.resource.source()),
                 None => continue,
             }
         }
+    }
+}
+
+/// Ensure that no removings are performed during the lifetime of the guard.
+pub struct ProtectedResource<'a, S: Resource> {
+    register: Arc<Register<S>>,
+    poll_registry: &'a PollRegistry,
+}
+
+impl<'a, S: Resource> ProtectedResource<'a, S> {
+    fn new(register: Arc<Register<S>>, poll_registry: &'a PollRegistry) -> Self {
+        register.delayed_removal.store(true, Ordering::Relaxed);
+        Self { register, poll_registry }
+    }
+}
+
+impl<'a, S: Resource> Drop for ProtectedResource<'a, S> {
+    fn drop(&mut self) {
+        self.register.delayed_removal.store(false, Ordering::Relaxed);
+        if self.register.must_remove.load(Ordering::Relaxed) {
+            ResourceRegistry::wait_to_remove(self.poll_registry, &mut self.register);
+        }
+    }
+}
+
+impl<'a, S: Resource> std::ops::Deref for ProtectedResource<'a, S> {
+    type Target = Arc<Register<S>>;
+
+    fn deref(&self) -> &Arc<Register<S>> {
+        &self.register
     }
 }

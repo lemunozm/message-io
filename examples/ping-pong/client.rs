db92d87e2ce2c7ec0a37bc9b06d09f1c88b230c7
lemunozm <lemunozm@gmail.com>
2021-03-30 20:51:43 +0200

    Fixed ping pong client

--- examples/ping-pong/client.rs
+++ examples/ping-pong/client.rs
@@ -1,53 +1,53 @@
 use super::common::{FromServerMessage, FromClientMessage};
 
-use message_io::network::{Network, NetEvent, Transport, RemoteAddr};
+use message_io::network::{NetEvent, Transport, RemoteAddr};
+use message_io::node::{Node, NodeEvent};
 
 use std::time::{Duration};
 
-enum Event {
-    Network(NetEvent),
-
+enum Signal {
     // This is a self event called every second.
-    // You can mix network events with your own events in the EventQueue.
-    // For that, call Network::split_and_map() to map to your custom event.
     Greet,
+    // Other signals here,
 }
 
 pub fn run(transport: Transport, remote_addr: RemoteAddr) {
-    let (network, mut events) = Network::split_and_map(|net_event| Event::Network(net_event));
+    let (handler, node) = Node::split();
 
-    let (server_id, local_addr) = match network.connect(transport, remote_addr.clone()) {
-        Ok(conn_info) => conn_info,
+    let server_id = match handler.network().connect(transport, remote_addr.clone()) {
+        Ok((server_id, local_addr)) => {
+            println!("Connected to server by {} at {}", transport, server_id.addr());
+            println!("Client identified by local port: {}", local_addr.port());
+            server_id
+        }
         Err(_) => {
             return println!("Can not connect to the server by {} to {}", transport, remote_addr)
         }
     };
 
-    println!("Connected to server by {} at {}", transport, server_id.addr());
-    println!("Client identified by local port: {}", local_addr.port());
-    events.sender().send(Event::Greet);
+    handler.signals().send(Signal::Greet);
 
-    loop {
-        match events.receive() {
-            Event::Greet => {
+    node.run(move |event| match event {
+        NodeEvent::Signal(signal) => match signal {
+            Signal::Greet => {
                 let message = FromClientMessage::Ping;
                 let output_data = bincode::serialize(&message).unwrap();
-                network.send(server_id, &output_data);
-                events.sender().send_with_timer(Event::Greet, Duration::from_secs(1));
+                handler.network().send(server_id, &output_data);
+                handler.signals().send_with_timer(Signal::Greet, Duration::from_secs(1));
             }
-            Event::Network(net_event) => match net_event {
-                NetEvent::Message(_, input_data) => {
-                    let message: FromServerMessage = bincode::deserialize(&input_data).unwrap();
-                    match message {
-                        FromServerMessage::Pong(count) => {
-                            println!("Pong from server: {} times", count)
-                        }
-                        FromServerMessage::UnknownPong => println!("Pong from server"),
+        },
+        NodeEvent::Network(net_event) => match net_event {
+            NetEvent::Message(_, input_data) => {
+                let message: FromServerMessage = bincode::deserialize(&input_data).unwrap();
+                match message {
+                    FromServerMessage::Pong(count) => {
+                        println!("Pong from server: {} times", count)
                     }
+                    FromServerMessage::UnknownPong => println!("Pong from server"),
                 }
-                NetEvent::Connected(_, _) => unreachable!(), // Only generated when listen
-                NetEvent::Disconnected(_) => return println!("Server is disconnected"),
-            },
+            }
+            NetEvent::Connected(_, _) => unreachable!(), // Only generated when listen
+            NetEvent::Disconnected(_) => return println!("Server is disconnected"),
         }
-    }
+    }).wait();
 }

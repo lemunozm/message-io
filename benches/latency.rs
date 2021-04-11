7db1b48d85cb096f903713fd8b7bc269cb1a9e8f
lemunozm <lemunozm@gmail.com>
2021-04-11 17:27:39 +0200

    Added native latencies

--- benches/latency.rs
+++ benches/latency.rs
@@ -1,13 +1,19 @@
 use message_io::network::{self, Transport, NetworkController, NetworkProcessor, Endpoint};
 use message_io::util::thread::{NamespacedThread};
+use message_io::util::encoding::{self, Decoder, MAX_ENCODED_SIZE};
 
 use criterion::{criterion_group, criterion_main, Criterion};
 
+use tungstenite::{Message, connect as ws_connect, accept as ws_accept};
+use url::{Url};
+
+use std::net::{TcpListener, TcpStream, UdpSocket};
 use std::time::{Duration};
 use std::sync::{
     Arc,
     atomic::{AtomicBool, Ordering},
 };
+use std::io::{Write, Read};
 
 lazy_static::lazy_static! {
     static ref TIMEOUT: Duration = Duration::from_millis(100);
@@ -49,6 +55,90 @@ fn latency_by(c: &mut Criterion, transport: Transport) {
     });
 }
 
+fn latency_by_native_udp(c: &mut Criterion) {
+    let msg = format!("latency by native Udp");
+    c.bench_function(&msg, |b| {
+        let receiver = UdpSocket::bind("127.0.0.1:0").unwrap();
+        let addr = receiver.local_addr().unwrap();
+
+        let sender = UdpSocket::bind("127.0.0.1:0").unwrap();
+        sender.connect(addr).unwrap();
+
+        let mut buffer: [u8; 1] = [0; 1];
+
+        b.iter(|| {
+            sender.send(&[0xFF]).unwrap();
+            receiver.recv(&mut buffer).unwrap();
+        });
+    });
+}
+
+fn latency_by_native_tcp(c: &mut Criterion) {
+    let msg = format!("latency by native Tcp");
+    c.bench_function(&msg, |b| {
+        let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+        let addr = listener.local_addr().unwrap();
+
+        let mut sender = TcpStream::connect(addr).unwrap();
+        let (mut receiver, _) = listener.accept().unwrap();
+
+        let mut buffer: [u8; 1] = [0; 1];
+
+        b.iter(|| {
+            sender.write(&[0xFF]).unwrap();
+            receiver.read(&mut buffer).unwrap();
+        });
+    });
+}
+
+fn latency_by_native_framed_tcp(c: &mut Criterion) {
+    let msg = format!("latency by native FramedTcp");
+    c.bench_function(&msg, |b| {
+        let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+        let addr = listener.local_addr().unwrap();
+
+        let mut sender = TcpStream::connect(addr).unwrap();
+        let (mut receiver, _) = listener.accept().unwrap();
+
+        let mut buffer: [u8; 1] = [0; 1];
+        let mut framming = [0; MAX_ENCODED_SIZE]; // used to avoid a heap allocation
+        let mut decoder = Decoder::default();
+
+        b.iter(|| {
+            let encoded_size = encoding::encode_size(&[0xFF], &mut framming);
+            sender.write(&encoded_size).unwrap();
+            sender.write(&[0xFF]).unwrap();
+            let received = receiver.read(&mut buffer).unwrap();
+            decoder.decode(&buffer[0..received], |_decoded_data| ());
+        });
+    });
+}
+
+use std::sync::Mutex;
+fn latency_by_native_web_socket(c: &mut Criterion) {
+    let msg = format!("latency by native Ws");
+    c.bench_function(&msg, |b| {
+        let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+        let addr = listener.local_addr().unwrap();
+
+        let mut listen_thread = NamespacedThread::spawn("perf-listening", move || {
+            ws_accept(listener.accept().unwrap().0).unwrap()
+        });
+
+        let url_addr = format!("ws://{}/socket", addr);
+        let (mut sender, _) = ws_connect(Url::parse(&url_addr).unwrap()).unwrap();
+
+        let mut receiver = listen_thread.join();
+
+        let message = vec![0xFF];
+
+        b.iter(|| {
+            sender.write_message(Message::Binary(message.clone())).unwrap();
+            receiver.read_message().unwrap();
+        });
+    });
+}
+
 fn latency(c: &mut Criterion) {
     #[cfg(feature = "udp")]
     latency_by(c, Transport::Udp);
@@ -58,6 +148,15 @@ fn latency(c: &mut Criterion) {
     latency_by(c, Transport::FramedTcp);
     #[cfg(feature = "websocket")]
     latency_by(c, Transport::Ws);
+
+    #[cfg(feature = "udp")]
+    latency_by_native_udp(c);
+    #[cfg(feature = "tcp")]
+    latency_by_native_tcp(c);
+    #[cfg(feature = "tcp")]
+    latency_by_native_framed_tcp(c);
+    #[cfg(feature = "websocket")]
+    latency_by_native_web_socket(c);
 }
 
 criterion_group!(benches, latency);
